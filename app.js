9e5057d58f9ecbe7b79f0379a9bbae96124a50e4
sahal-shalu <sahalshalu5426@gmail.com>
2024-02-08 11:23:38 +0530

    updated

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express=require('express')
 const app=express()
 const indexrouter=require('./routes/index')
+const bodyparser=require('body-parser')
+
+
 
 //To take static files from public directory
 app.use(express.static('public'))
+
+app.use(bodyparser.urlencoded({extended:true}))
 
 // use rout handlers
 app.use('/',indexrouter)
 
 // Start the server
-app.listen(4000,()=>{
-    console.log('http://localhost:4000')
+app.listen(3000,()=>{
+    console.log('http://localhost:3000')
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express=require('express')
 const router=express.Router()
+const path=require('path')
 
+
+let todos=[]
+
+
+
+router.post('/add',(req,res)=>{
+    const {task}=req.body;
+    todos.push({id:Date.now(),task})
+    res.redirect('/')
+})
+
+router.delete('/delete/:id',(req,res)=>{
+    const{id}=req.params;
+    todos=todos.filter(todo=>todo.id!==parseInt(id))
+    res.json(todos)
+})
 
 router.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/views/index.html")
+    res.sendFile(path.join(__dirname,"../views/index.html"))
 })
 
 module.exports=router;
